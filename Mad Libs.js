5590ec016c58b8875c4c01c4d07ef56706b5997b
sthrasher17 <32141005+sthrasher17@users.noreply.github.com>
2017-10-02 11:18:04 -0400

    Add files via upload

--- Mad Libs.js
+++ Mad Libs.js
@@ -1,15 +1,25 @@
+//initialize variables
 var sentence=[];
 var replacements=[];
 var index=0;
+//initialize user
 alert("Filler go away.")
 alert("End with Shift+6 (^)")
-//populate(index,sentence);
-alert (populate(index,sentence));
+//fill sentence
+populate(index,sentence, "word");
+index=0;
+alert(sentence);
+//specify positions
+populate(index,replacements, "position");
+alert(replacements);
+index=0;
+populate(index,sentence,replacements,"part of speech");
+alert(sentence);
 
 
-function populate(sindex, sarray){
+function populate(sindex, sarray, label) {
 	do	{
-		sarray[sindex]=prompt("Next word?");
+		sarray[sindex]=prompt("Next "+label+":");
 		sindex = sindex+1;
 	}
 	while (sarray[sindex-1]!="^");
@@ -17,3 +27,11 @@ function populate(sindex, sarray){
 	sarray.splice(-1,1);
 	return sarray;
 }
+
+function replace(sindex,sarray,rarray,label){
+	for (sindex = 0; sindex < rarray.length; sindex++) {
+	sarray[rarray[sindex]] = prompt("Next "+label+"\:");
+	}
+	return sarray;
+}
+
