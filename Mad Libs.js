74886a2eb2a65ece380011ea83900b351ef0dbbf
sthrasher17 <32141005+sthrasher17@users.noreply.github.com>
2017-10-05 08:10:24 -0400

    Add files via upload

--- Mad Libs.js
+++ Mad Libs.js
@@ -13,10 +13,18 @@ alert(sentence);
 populate(index,replacements, "position");
 alert(replacements);
 index=0;
-populate(index,sentence,replacements,"part of speech");
-alert(sentence);
-
-
+replace(index,sentence,replacements,"part of speech");
+//switch people
+//train filler
+alert("Enter words that match part of speech shown.");
+//enter replacement words
+index=0;
+replace(index,sentence,replacements, "new word");
+//show the sentence
+alert("Go get the author to see the sentence!");
+sentenceString = sentence.join(' ');
+alert(sentenceString.toString());
+index=0;
 function populate(sindex, sarray, label) {
 	do	{
 		sarray[sindex]=prompt("Next "+label+":");
@@ -30,8 +38,12 @@ function populate(sindex, sarray, label) {
 
 function replace(sindex,sarray,rarray,label){
 	for (sindex = 0; sindex < rarray.length; sindex++) {
-	sarray[rarray[sindex]] = prompt("Next "+label+"\:");
+		if(label == "new word"){
+			sarray[rarray[sindex]] = prompt("Provide a "+sarray[rarray[sindex]]+" here: ");
+		}
+		else {
+			sarray[rarray[sindex]] = prompt("Next "+label+":");
+		}
 	}
 	return sarray;
 }
-
